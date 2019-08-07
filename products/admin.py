bfdeb2b14c874345b95fdc8c83119f7a19946025
nitrogendragon <dirtboy4fun@gmail.com>
2019-08-07 16:34:14 -0500

    Basic website that displays products with their images, name, price and an add to cart button that doesn't actually do anything.

--- products/admin.py
+++ products/admin.py
@@ -1,3 +1,16 @@
 from django.contrib import admin
+from .models import Product, Offer
+
+
+class ProductAdmin(admin.ModelAdmin):
+    list_display = ('name', 'price', 'stock')
+
+
+class OfferAdmin(admin.ModelAdmin):
+    list_display = ('code', 'discount')
+
+
 
 # Register your models here.
+admin.site.register(Product, ProductAdmin)
+admin.site.register(Offer, OfferAdmin)
--- products/views.py
+++ products/views.py
@@ -1,13 +1,24 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+from .models import Product
 
 # /products -> index
 #Uniform Resource Locator aka address
 
-#functions for dealing with requests that basically tell django what to display on the page
-def index(request):
-    return HttpResponse('Hello World')
 
+#functions for dealing with requests that basically tell django what to display on the page
+
+
+def index(request):
+    products = Product.objects.all() # get all products
+    # Product.objects.filter() #filter by cost or stock etc
+    # Product.objects.get()#get a single product
+    # Product.objects.save #insert or update new/existing product
+    #return HttpResponse('Hello World')
+    #call render function with the server request, the template file name, and a dictionary that contains
+    #the data to be passed to the template
+    return render(request, 'index.html',
+                  {'products': products})
 
 def new(request):
     return HttpResponse('New Products')
