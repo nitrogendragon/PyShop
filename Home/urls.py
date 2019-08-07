7711e528bfbc0176805d0729b020ecb35fa24851
nitrogendragon <dirtboy4fun@gmail.com>
2019-08-07 17:38:51 -0500

    Starting to work on Home Page

---
+++ Home/urls.py
@@ -0,0 +1,9 @@
+from django.urls import path
+from . import views
+
+#setting up paths so django knows what view to show based on what we enter after products/
+# so products/   will take us to views.index
+# and products/new will take us to views.new and thusly show us the respective pages
+urlpatterns = [
+    path('', views.index)
+]
--- pyshop/settings.py
+++ pyshop/settings.py
@@ -37,7 +37,8 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'products.apps.ProductsConfig'#adding our products app
+    'products.apps.ProductsConfig',#adding our products app
+    'Home.apps.HomeConfig'
 ]
 
 MIDDLEWARE = [
@@ -56,7 +57,8 @@ TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
         'DIRS': [
-            os.path.join(BASE_DIR, 'templates')
+            os.path.join(BASE_DIR, 'templates'),
+
         ],
         'APP_DIRS': True,
         'OPTIONS': {
